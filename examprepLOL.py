b2bae5f0fa5c1366f71f9ef062eb30807f525ac8
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-21 16:46:58 -0500

    comments

--- examprepLOL.py
+++ examprepLOL.py
@@ -6,3 +6,14 @@ Created on Wed Feb 21 16:41:25 2024
 @author: stephenson
 """
 
+N = 0
+
+while N <= 10:
+#the above line is essentilly stating that if the var N is above 10, which 
+#in this particular case it is...it's been set to zero; execute the given 
+#control structure
+    print(N)
+    #so, whatever is stored at the counter before the data at the var is
+    #changed with the code at the statement below
+    #is logged to the terminal emulator before it's actually modified
+    N = N + 2
